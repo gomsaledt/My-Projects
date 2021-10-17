7f38a6f023be370e7ad2675ed062f813a3504ce4
gomsaledt <gomsaledt19.comp@coep.ac.in>
2021-10-17 20:28:23 +0530

    AlertBox.py added to MiniProjects

---
+++ MiniProjects/AlertBox.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3 
+
+# --------------- A program to display an alert box with message -----------------
+
+import pyautogui
+
+num = int(input("Enter a value to divide 100: "))
+
+if num == 0:
+    pyautogui.alert(" Alert!!! 100 cannot be divided by 0")
+else:
+    print(f"The value is {100/num}")
